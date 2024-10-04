57719fa030be90c16983d807ebde84bc51013024
hndrapratamaa <143484837+hndrapratamaa@users.noreply.github.com>
2024-10-05 06:14:54 +0700

    Add files via upload

---
+++ assets/scripts/helpers.js
@@ -0,0 +1,90 @@
+const selectElem = elem => {
+  return document.querySelector(elem);
+}
+
+const modifyElem = (elem, obj) => {
+  const element = document.querySelector(elem);
+
+  const existsAttributes = obj.attributes;
+  const existsStyles = obj.styles;
+
+  if (existsAttributes) {
+    const attributes = Object.entries(obj.attributes);
+
+    for (const attribute of attributes) {
+      const [attrName, attrValue] = attribute;
+
+      element.setAttribute(attrName, attrValue);
+    }
+
+  }
+
+  if (existsStyles) {
+    const allStyles = Object.entries(obj.styles);
+    const parsedStyles = [];
+
+    allStyles.forEach(style => {
+      const [ property, value ] = style;
+
+      const parsedStyle = `${property}: ${value};`
+
+      parsedStyles.push(parsedStyle);
+    });
+
+    element.style.cssText = parsedStyles.join(" ");
+
+  }
+  
+};
+
+const createElem = (elem, obj) => {
+  const element = document.createElement(elem);
+
+  const existsAttributes = obj.attributes;
+  const existsStyles = obj.styles;
+  const haveTextContent = obj.text;
+
+  if (existsAttributes) {
+    const attributes = Object.entries(obj.attributes);
+
+    for (const attribute of attributes) {
+      const [attrName, attrValue] = attribute;
+
+      element.setAttribute(attrName, attrValue);
+    }
+
+  }
+
+  if (existsStyles) {
+    const allStyles = Object.entries(obj.styles);
+    const parsedStyles = [];
+
+    allStyles.forEach(style => {
+      const [ property, value ] = style;
+
+      const parsedStyle = `${property}: ${value};`
+
+      parsedStyles.push(parsedStyle);
+    });
+
+    element.style.cssText = parsedStyles.join(" ");
+
+  }
+
+  if (haveTextContent) {
+    element.textContent = obj.text;
+  }
+
+  return element;
+};
+
+const hideElem = (elem, bool) => {
+  if (bool) {
+    return elem.classList.add("none");
+  }
+
+  elem.classList.remove("none");
+
+};
+
+export { selectElem, modifyElem, createElem, hideElem }
